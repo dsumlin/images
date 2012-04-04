ece64943964f300dc33674016d79e26697b56284
dsumlin <webmaster@dennissumlin.com>
2012-04-04 11:28:12 -0400

    h

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ PaperclipRails3::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-  config.assets.compile = true
+  #config.assets.compile = true
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
