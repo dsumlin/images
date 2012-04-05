a45c77dc1786b829e4bcee9159eebef6cb65c821
dsumlin <webmaster@dennissumlin.com>
2012-04-05 11:15:29 -0400

    h

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ActiveRecord::Base  
     attr_accessible :title, :content, :assets_attributes
     has_many :assets
-    accepts_nested_attributes_for :assets, :allow_destroy => true    
+    accepts_nested_attributes_for :assets, :allow_destroy => true  
+    has_attached_file :photo,
+        :storage => :s3,
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :s3_credentials => {
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }  
 end
