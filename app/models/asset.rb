caab86d3a432884e74439599d4f26fac46a5adfa
dsumlin <webmaster@dennissumlin.com>
2012-04-07 11:24:53 -0400

    h

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,11 @@
 class Asset < ActiveRecord::Base
   belongs_to :post
   has_attached_file :asset, :styles => { :large => "640x480", :medium => "300x300>", :thumb => "100x100>" }
+   has_attached_file :photo,
+        :storage => :s3,
+        :bucket => ENV['StarScream'],
+        :s3_credentials => {
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
 end
