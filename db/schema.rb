cba354b7d200a9989df12bcf27a9677bec2cfbce
dsumlin <webmaster@dennissumlin.com>
2012-04-06 21:28:28 -0400

    h

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406225654) do
+ActiveRecord::Schema.define(:version => 20120407012704) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(:version => 20120406225654) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", :force => true do |t|
